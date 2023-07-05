3050af8d4dca9cfac801e4dce34bb22e11cbe206
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2023-07-05 23:01:20 +0200

    fix ci

--- Build/build.bat
+++ Build/build.bat
@@ -1,3 +1,7 @@
+echo off
 
+set BUILD_COMMAND=%CD%\..\tools\util\make\make.exe -f %CD%\..\Make\make_000.gmk all TYP_OS=win
 
-%CD%\..\tools\util\make\make.exe -f %CD%\..\Make\make_000.gmk all
+echo Build command is: %BUILD_COMMAND%
+
+%BUILD_COMMAND%
