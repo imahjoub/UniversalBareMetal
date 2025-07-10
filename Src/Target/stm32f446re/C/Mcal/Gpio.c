62f1b92e359283ec7b4ddcae6ef0b44cca36f839
Iliass Mahjoub <mahjoub.iliass@gmail.com>
2025-07-10 23:08:07 +0200

    implement pwm in stm32f446re

--- Src/Target/stm32f446re/C/Mcal/Gpio.c
+++ Src/Target/stm32f446re/C/Mcal/Gpio.c
@@ -9,14 +9,35 @@ void GPIO_Init(void)
   GPIOA_MODER |=  (1UL << (USER_LED * 2U));
   GPIOA_MODER &= (uint32_t)(~(1UL << (USER_LED * 2U + 1U)));
 
-  /* Configure USER_BUTTON as input (00) */
+  /* Configure USER_BUTTON PC13 as input (00) */
   GPIOC_MODER &= (uint32_t)(~((1UL << (USER_BUTTON * 2U)) | (1UL << (USER_BUTTON * 2U + 1U))));
 
-  /* Set USER_BUTTON as pull-down (10) */
+  /* Set USER_BUTTON PC13 as pull-down (10) */
   GPIOC_PUPDR |= (1UL << (USER_BUTTON * 2U + 1U));
   GPIOC_PUPDR &= (uint32_t)(~(1UL << (USER_BUTTON * 2U)));
 
+  /* Configure USER_BUTTON PC3 as input */
+  //GPIOC_MODER &= (uint32_t)(~((1UL << (WAKEUP_PIN * 2U)) | (1UL << (WAKEUP_PIN * 2U + 1U))));
+  /* Set USER_BUTTON PC13 no pull */
+  //GPIOC_PUPDR &= (uint32_t)(~(1UL << (WAKEUP_PIN * (2U + 1U))));
+  //GPIOC_PUPDR &= (uint32_t)(~(1UL << (WAKEUP_PIN * (2U + 0U))));
+
+
+  /* configure the PA0 to  for TIM2_CH1 */
+  GPIOA_MODER &= (uint32_t)(~(1UL << 0U));
+  GPIOA_MODER |= (uint32_t)(1UL << 1U);
+
+  /* configure the PA1 to  for TIM2_CH2 */
+  GPIOA_MODER &= (uint32_t)(~(1UL << 2U));
+  GPIOA_MODER |= (uint32_t)(1UL << 3U);
+
+  /* Set alternate function type to TIM2 (AF01) */
+  GPIOA_AFRL &= (uint32_t)(~(0xFUL << 0U));
+  GPIOA_AFRL &= (uint32_t)(~(0xFUL << 4U));
+  GPIOA_AFRL |= (uint32_t)(0x1UL << 0U);
+  GPIOA_AFRL |= (uint32_t)(0x1UL << 4U);
 }
+
 
 void Led_Blinky(void)
 {
